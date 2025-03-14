b1264635ae2c591bc691f749e6aeb06934b4bd50
Wandile-lab <wandilen158@gmail.com>
2025-03-14 10:23:45 +0200

    Correct Spelling error

--- script.js
+++ script.js
@@ -67,5 +67,16 @@ resetBtn.addEventListener("click", () => {
     buttonColorShuffle(); 
 });
 
+//time feature
+currentTimeUTC = () => {
+    const time = document.getElementById("time")
+    const date = new Date();
+    const utcTime = date.toISOString().split("T")[1].split(".")[0];
+    time.textContent = `Current Time (UTC): ${utcTime}`;
+}
+
+window.onload = currentTimeUTC;
+
+
 
 
